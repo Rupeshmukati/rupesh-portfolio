76fea5e98aac7316b6bc8dbb24966f2d6cf1b4ff
Rupeshmukati <33753207+Rupeshmukati@users.noreply.github.com>
2026-01-14 14:11:03 +0530

    Initial MERN portfolio project

--- server.js
+++ server.js
@@ -21,6 +21,16 @@ app.get("/", (req, res) => {
 
 // Server
 const PORT = process.env.PORT || 5000;
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+           
