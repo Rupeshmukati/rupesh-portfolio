ef01315366a4d34f0b45ab6c89fb3ddaa9a6aae2
Rupeshmukati <33753207+Rupeshmukati@users.noreply.github.com>
2026-01-14 14:53:48 +0530

    Fix express fallback route for Render (Express v5)

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("/*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
