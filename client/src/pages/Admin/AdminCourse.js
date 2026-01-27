2604a1b3b8b9fa8a3793c820bbfa56fe6a48fd33
Rupeshmukati <33753207+Rupeshmukati@users.noreply.github.com>
2026-01-27 18:35:43 +0530

    Updated frontend: client folder changes, including index.html

--- client/src/pages/Admin/AdminCourse.js
+++ client/src/pages/Admin/AdminCourse.js
@@ -122,7 +122,7 @@ function AdminCourses() {
             </div>
 
             {/* Content */}
-            <p className="text-primary font-semibold pt-4">{course.title}</p>
+            <p className="text-primary font-semibold pt-4 mt-3">{course.title}</p>
             <img
               src={course.image}
               alt={course.title}
